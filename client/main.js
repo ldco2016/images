b84a773924398b60f73aa309afea52c8a77c2407
danale <danale@danales-MacBook-Pro.local>
2019-10-12 00:08:15 -0500

    Implemented class-based component

--- client/main.js
+++ client/main.js
@@ -1,19 +1,29 @@
 // import the React library
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import ImageList from "./components/image_list";
 import "./main.html";
 
 // Create a Component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
+}
 
 // Render this component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector(".container"));
+  axios
+    .get("https://api.imgur.com/3/gallery/hot/viral/0", {
+      headers: {
+        Authorization: "Client-ID 634743603ce28f3"
+      }
+    })
+    .then(response => console.log(response));
 });
